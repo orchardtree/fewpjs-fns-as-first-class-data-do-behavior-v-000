d8fa526f90931b7ce42ed6dc8deee2594295dc4b
Jesse Wenger <wengerj@gmail.com>
2021-05-31 14:37:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,13 +13,10 @@ let web_greeting = document.querySelector("h1#greeting");
 
 /* Write your implementation of greet() */
 function greet() {
-  if (time_field == "test1") {
+  if (time_field == "test1")
     return "Good Morning";
-  else if (time_field == "test2")
-    return "Good Afternoon";
   else
     return "Good Evening";
-  }
 }
 
 /* Write your implementation of displayMessage() */
