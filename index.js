20e241fdf1b263229ccde68792433938700bcb4a
Jesse Wenger <wengerj@gmail.com>
2021-05-31 15:17:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 /* Write your implementation of greet() */
 function greet(timeString) {
-
+  return timeString
 }
 
 /* Write your implementation of displayMessage() */
