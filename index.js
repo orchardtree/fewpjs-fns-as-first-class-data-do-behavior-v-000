17381f819314eeb09bb58ae57180ac877a9ac9c2
Jesse Wenger <wengerj@gmail.com>
2021-05-31 19:47:53 +0000

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
-
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let greetingString = 'ERROR'
+  const hourString = timeString.split(':')[0];
+  const hourInt = parseInt(hourString);
+  if (hourInt < 12) {
+        greetingString = 'Good Morning';
+    } else if (hourInt >= 12 && hourInt <= 17) {
+        greetingString = 'Good Afternoon';
+    } else if (hourInt >= 17 && hourInt < 24) {
+        greetingString = 'Good Evening';
+    } else {
+        greetingString = timeString
+  }
+  return greetingString
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(greetingString) {
+  const greetingNode = document.getElementById('greeting')
+  greetingNode.innerText = greetingString
+}
