f8737bbb5c63271dbd74d2c23d861226a6ac0585
Jesse Wenger <wengerj@gmail.com>
2021-05-31 14:21:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let web_greeting = document.querySelector("h1#greeting");
 
 /* Write your implementation of greet() */
 function greet() {
-  "Good Morning"
+  return "Good Morning"
 }
 
 /* Write your implementation of displayMessage() */
