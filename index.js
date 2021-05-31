e6f72e7cc346246865482d6fa509f684197e2855
Jesse Wenger <wengerj@gmail.com>
2021-05-31 17:25:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 function greet() {
   const timeString = document.getElementById('time').value
-  const  =
+  return timeString
 }
 
 /* Write your implementation of displayMessage() */
