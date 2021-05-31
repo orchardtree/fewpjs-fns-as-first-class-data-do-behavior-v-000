4582aa84adaed025a2faaa808951523a38b10909
Jesse Wenger <wengerj@gmail.com>
2021-05-31 15:03:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function greet() {
 }
 
 /* Write your implementation of displayMessage() */
-function displayMessage(string) {
+function displayMessage(timeString) {
   h1Greeting.innerHTML = greet();
 }
