97fae1483899a8d8d4eeec45df7359e5fc1b49e5
Jesse Wenger <wengerj@gmail.com>
2021-05-31 18:33:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function greet(timeString) {
   const hourString = timeString.split(':')[0];
   const parsed = parseInt(hourString);
   if (parsed < 12) {
-    let greetingString = 'Good Morning';
+    greetingString = 'Good Morning';
   }
   /* } else if (parsed >= 12 && parsed <= 17) {
       greetingString = "Good Afternoon";
