eeb0f6c6830788b5f0240f1d3c6ee66295ae3765
Jesse Wenger <wengerj@gmail.com>
2021-05-31 18:33:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 /* Write your implementation of greet() */
 function greet(timeString) {
-  let greetingString = "ERROR"
+  let greetingString = 'ERROR'
   // const timeString = document.getElementById('time').value;
   const hourString = timeString.split(':')[0];
   const parsed = parseInt(hourString);
