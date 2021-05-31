bc3070a9f18e72533296d85dd05621aeb027800c
Jesse Wenger <wengerj@gmail.com>
2021-05-31 19:07:58 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,34 @@
+/* Given Code, don't edit */
+
+document.querySelector("button").addEventListener("click", handleClick);
+
+function handleClick(e) {
+  const timeString = document.getElementById('time').value
+  displayMessage(greet(timeString))
+}
+
+/* End Given Code, don't edit above here...*/
+/* Write your implementation of greet() */
+function greet(timeString) {
+  let greetingString = 'ERROR'
+  const hourString = timeString.split(':')[0];
+  const parsed = parseInt(hourString);
+  if (parsed < 12) {
+    greetingString = 'Good Morning';
+  } else if (parsed >= 12 && parsed <= 17) {
+      greetingString = 'Good Afternoon';
+  } else {
+      greetingString = 'Good Evening';
+  }
+  return greetingString
+}
+
+/* Write your implementation of displayMessage() */
+function displayMessage(timeString) {
+  const greetingNode = document.getElementById('greeting')
+  if timestring != [0...24] {
+    "TEST";
+  else
+    greetingNode.innerHTML = greet(timeString)
+  }
+}
