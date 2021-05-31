328e63d41c0c8accb84717a64dc248e6b7dc23be
Jesse Wenger <wengerj@gmail.com>
2021-05-31 19:15:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ function greet(timeString) {
 
 /* Write your implementation of displayMessage() */
 function displayMessage() {
+  const greetingNode = document.getElementById('greeting')
+  greetingNode.innerHTML = greet(timeString);  
 /*  const greetingNode = document.getElementById('greeting')
   if timestring != [0...24] {
     "TEST";
